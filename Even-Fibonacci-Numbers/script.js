94a33bc7505409b00117c6731edb4014d2234784
AlexandrKilyakov <76633175+AlexandrKilyakov@users.noreply.github.com>
2021-09-06 16:48:43 +0300

    Update script.js

--- Even-Fibonacci-Numbers/script.js
+++ Even-Fibonacci-Numbers/script.js
@@ -1,14 +1,13 @@
-var input = document.querySelector('#number')
-var click = document.querySelector('#btn')
-var result = document.querySelector('#result')
+var input = document.querySelector('#number');
+var click = document.querySelector('#btn');
+var result = document.querySelector('#result');
 
 click.addEventListener("click", function(e) 
 {
 	e.preventDefault();
-	var n = input.value;
-	FN = [0, 2]
-	for(var i = 1; i < n; i++)
-		FN.push(4 * FN[i] + FN[i - 1]);
+	let n = input.value;
+	let FN = [0, 2];
+	for(let i = 1; i < n; i++) FN.push(4 * FN[i] + FN[i - 1]);
 	
 	result.innerHTML = FN.join(', ');
 });
